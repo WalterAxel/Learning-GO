407effd3788b10609965f5efe50ff9a232daff78
WalterAxel <walter.wahlroos@gmail.com>
2023-02-20 15:08:41 +0200

    structs, methods and interfaces learned. also fixed the go.work file

---
+++ structs, methods and interfaces/shapes_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{shape: Rectangle{Width: 10.0, Height: 10.0}, hasArea: 100.0},
+		{shape: Circle{Radius: 5.0}, hasArea: 78.53981633974483},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the 't.Run' test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
